9b273c7def4e47428e5f4cd7b25e993c196c54f1
Alian-Andahur5 <alian.andahur.lml@gmail.com>
2017-05-29 23:41:33 -0300

    layouts move

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
               }
 
         </style>
-
+      
 
         @yield('head')
 
@@ -34,15 +34,18 @@
       @yield('content')
 
       <script src="js.jquery.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
       <script
         src="https://code.jquery.com/jquery-3.1.1.min.js"
         integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
         crossorigin="anonymous">
       </script>
-
-
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+      <script>
+      $(document).ready(function(){
+          console.log("test");
+        $('.parallax').parallax();
+      });
+      </script>
 
         @yield('scripts')
 
