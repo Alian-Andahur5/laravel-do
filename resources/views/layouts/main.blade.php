edcad6f2b80aa3a7a38d93921124fc924f56c354
Alian-Andahur5 <alian.andahur.lml@gmail.com>
2017-06-01 03:18:37 -0300

    plantilla correcta

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,51 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Parallax Template - Materialize</title>
 
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <style type="text/css">
-
-             .hide {
-                  display:none;
-              }
-
-        </style>
-      
-
-        @yield('head')
-
-    </head>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
     <body>
-      <nav>
-          <div class="nav-wrapper">
-          <a href="#" class="brand-logo"></a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="registro">Registrarse</a></li>
-              <li><a href="login">Iniciar sesion</a></li>
+
+      <nav class="white" role="navigation">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="#" class="brand-logo">Logo</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">Navbar Link</a></li>
           </ul>
-          </div>
+
+          <ul id="nav-mobile" class="side-nav">
+            <li><a href="#">Navbar Link</a></li>
+          </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
       </nav>
+
+
 
       @yield('content')
 
-      <script src="js.jquery.js"></script>
-      <script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous">
-      </script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-      <script>
-      $(document).ready(function(){
-          console.log("test");
-        $('.parallax').parallax();
-      });
-      </script>
+
+<footer class="page-footer teal">
+   <div class="container">
+     <div class="row">
+       <div class="col l6 s12">
+         <h5 class="white-text">Company Bio</h5>
+         <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+       </div>
+       <div class="col l3 s12">
+         <h5 class="white-text">Settings</h5>
+         <ul>
+           <li><a class="white-text" href="#!">Link 1</a></li>
+           <li><a class="white-text" href="#!">Link 2</a></li>
+           <li><a class="white-text" href="#!">Link 3</a></li>
+           <li><a class="white-text" href="#!">Link 4</a></li>
+         </ul>
+       </div>
+       <div class="col l3 s12">
+         <h5 class="white-text">Connect</h5>
+         <ul>
+           <li><a class="white-text" href="#!">Link 1</a></li>
+           <li><a class="white-text" href="#!">Link 2</a></li>
+           <li><a class="white-text" href="#!">Link 3</a></li>
+           <li><a class="white-text" href="#!">Link 4</a></li>
+         </ul>
+       </div>
+     </div>
+   </div>
+   <div class="footer-copyright">
+     <div class="container">
+     Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+     </div>
+   </div>
+ </footer>
+
+
+
+       <!--  Scripts-->
+      <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script src="js/materialize.js"></script>
+      <script src="js/init.js"></script>
 
         @yield('scripts')
 
