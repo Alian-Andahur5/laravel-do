3bb2278d4393e61cf59fcc02108acea09a7ea004
Alian-Andahur5 <alian.andahur.lml@gmail.com>
2017-06-02 22:46:22 -0300

    socialbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,31 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-  <title>Parallax Template - Materialize</title>
+  <title>AlianAndahur.cl </title>
 
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/header.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/social.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/fonts.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
 </head>
     <body>
+      <div class="social">
 
-      <nav class="white" role="navigation">
-        <div class="nav-wrapper container">
-          <a id="logo-container" href="#" class="brand-logo">Logo</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="#">Navbar Link</a></li>
+          <ul>
+            <li><a href="http://www." target="_blank" class="icon-facebook"></a></li>
+            <li><a href="http://www." target="_blank" class="icon-googleplus"></a></li>
+            <li><a href="mailto:alian.andahur.lml@gmail.com"class="icon-mail"></a></li>
           </ul>
 
-          <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">Navbar Link</a></li>
-          </ul>
-          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-        </div>
-      </nav>
+      </div>
 
+      <div class="row">
+           <header>
+                <nav class="white" role="navigation">
+                  <div class="nav-wrapper container">
+                    <a id="logo-container" href="www.alianandahur.cl" class="brand-logo">World of guitars by </a>
+                    <ul class="right hide-on-med-and-down">
+                      <li><a href="#">Entrevistas</a></li>
+                      <li><a href="#">Galeria</a></li>
+                      <li><a href="#">Contacto</a></li>
+                    </ul>
+
+                    <ul id="nav-mobile" class="side-nav">
+                      <li><a href="#">Navbar Lin</a></li>
+                    </ul>
+                    <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+                  </div>
+                </nav>
+           </header>
+   <div>
 
 
       @yield('content')
@@ -41,18 +59,18 @@
 
        </div>
        <div class="col l3 s12">
-         <h5 class="white-text">Settings</h5>
+         <h5 class="white-text">Contacto</h5>
          <ul>
-           <li><a class="white-text" href="#!">Link 1</a></li>
+           <li><a class="white-text" href="#!">mail alian.andahur.lml@gmail.com</a></li>
            <li><a class="white-text" href="#!">Link 2</a></li>
            <li><a class="white-text" href="#!">Link 3</a></li>
            <li><a class="white-text" href="#!">Link 4</a></li>
          </ul>
        </div>
        <div class="col l3 s12">
-         <h5 class="white-text">Connect</h5>
+         <h5 class="white-text">Tambien puedes visitar</h5>
          <ul>
-           <li><a class="white-text" href="#!">Link 1</a></li>
+           <li><a class="white-text" href="https://www.youtube.com/channel/UCuRE9d41HODLJExP0qC7m_g/videos"></a></li>
            <li><a class="white-text" href="#!">Link 2</a></li>
            <li><a class="white-text" href="#!">Link 3</a></li>
            <li><a class="white-text" href="#!">Link 4</a></li>
@@ -62,7 +80,7 @@
    </div>
    <div class="footer-copyright">
      <div class="container">
-     Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+     © 2017 Copyright <a class="brown-text text-lighten-3" href="www.alianandahur.cl">www.alianandahur.cl</a>
      </div>
    </div>
  </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,18 +9,18 @@
   <div class="section no-pad-bot">
     <div class="container">
       <br><br>
-      <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
+      <h1 class="header center teal-text text-lighten-2">Alian Andahur</h1>
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">¡Descubre un mundo infinito a través de una simple guitarra!</h5>
       </div>
       <div class="row center">
-        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+        <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Comenzar lecciones</a>
       </div>
       <br><br>
 
     </div>
   </div>
-  <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
+  <div class="parallax"><img src="ali1.jpg" id="Ali1" alt="Unsplashed background img 1"></div>
 </div>
 
 
@@ -31,28 +31,27 @@
     <div class="row">
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-          <h5 class="center">Speeds up development</h5>
+          <h2 class="center brown-text"><i class="material-icons">done</i></h2>
+          <h5 class="center">Enfoque</h5>
 
-          <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+          <p class="light">¡Haz mucho con poco! La misión de cada lick o lesson está empeñada en lograr una buena capacidad del guitarrista a través de conceptos básicos y ejercicios simples.</p>
         </div>
       </div>
 
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-          <h5 class="center">User Experience Focused</h5>
+          <h2 class="center brown-text"><i class="material-icons">library_music</i></h2>
+          <h5 class="center">Desarrolla tus técnicas</h5>
 
-          <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+          <p class="light">A partir de cada lick, podrás opinar,  presentar dudas, proponer o preguntar acerca del ejercicio. Con el fin de ampliar la entrega de conocimientos, fomentanto un ambiente amigable de críticas constructivas.</p>
         </div>
       </div>
 
       <div class="col s12 m4">
         <div class="icon-block">
-          <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-          <h5 class="center">Easy to work with</h5>
-
-          <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+          <h2 class="center brown-text"><i class="material-icons">assessment</i></h2>
+          <h5 class="center">Conoce mi música</h5>
+          <p class="light">Estas cordialmente invitado(a) a escuchar y conocer mi música disponible en esta plataforma, adherido a entrevistas y experiencia de diversidad de artistas.</p>
         </div>
       </div>
     </div>
@@ -65,11 +64,11 @@
   <div class="section no-pad-bot">
     <div class="container">
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">No hay limites, siempre se aprende algo nuevo, !disfruta el contenido¡</h5>
       </div>
     </div>
   </div>
-  <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+  <div class="parallax"><img src="Ali1.jpg" id="Ali1" alt="Unsplashed background img 2"></div>
 </div>
 
 <div class="container">
@@ -78,31 +77,34 @@
     <div class="row">
       <div class="col s12 center">
         <h3><i class="mdi-content-send brown-text"></i></h3>
-        <h4>Contact Us</h4>
-        <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        <h4>Deja tu comentario</h4>
+        <div class="fb-comments" data-href="http://www.alianandahur.cl/" data-width="50px " data-numposts="5"></div>
+        <p class="left-align light"> ajhsdgjashdkajskhdkaaaaaaaaaaaaksjhdkajshdkjahsdkahsdjkhaskjhdakjshdkajhsdkjahsd</p>
+
       </div>
+
     </div>
 
   </div>
 </div>
+
 
 
 <div class="parallax-container valign-wrapper">
   <div class="section no-pad-bot">
     <div class="container">
       <div class="row center">
-        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+        <h5 class="header col s12 light">Sigamos aprendiendo, regresa pronto</h5>
       </div>
     </div>
   </div>
-  <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+  <div class="parallax"><img src="FD2.gif" alt="Unsplashed background img 3"></div>
 </div>
 
 
 
 @Stop
 @Section('Scripts')
-
 
 
 
